7eabdaed8a036938d695aa72a9d2a83edb0b8f06
enduraew <enduraew@gmail.com>
2023-08-07 18:54:02 +0500

    Refac feat light/dark mode

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
-const body = document.querySelector('body');
-// nav
-const nav = document.querySelector('nav');
+function setTheme() {
+  const root = document.documentElement;
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
+}
+
 const lightModeBtn = document.querySelector('.light-btn');
-const navLinks = nav.querySelectorAll('a');
-// header
-const header = document.querySelector('header');
-const headerH1 = header.querySelector('h1');
-// main
-const main = document.querySelector('main');
-const articles = main.querySelectorAll('article');
-const articlesLinks = main.querySelectorAll('a');
-const articlesH2 = main.querySelector('h2');
-
-// light/dark mode implementation
-lightModeBtn.addEventListener('click', () => {
-  body.classList.toggle('light-mode');
-  nav.classList.toggle('light-nav-border');
-  articlesH2.classList.toggle('light-articles-h2');
-  headerH1.classList.toggle('light-header-h1');
-  navLinks.forEach(item => {
-    item.classList.toggle('light-nav-links');
-  });
-  articlesLinks.forEach(item => {
-    item.classList.toggle('light-articles-a');
-  });
-  articles.forEach(item => {
-    item.classList.toggle('light-articles');
-  });
-});
-
-lightModeBtn.addEventListener('mousedown', () => {
-  lightModeBtn.classList.add('light-btn');
-});
+lightModeBtn.addEventListener('click', setTheme);
